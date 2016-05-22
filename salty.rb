63ea197bfc0f3ac9ae956c476eea3c035c3ee44c
Carlos Rodriguez <carlos@s8f.org>
2016-05-22 16:45:49 -0700

    salty 2.1.1 formula

---
+++ salty.rb
@@ -0,0 +1,13 @@
+class Salty < Formula
+  desc "Alternative public key encryption using NaCl"
+  homepage "https://github.com/carlos8f/salty"
+  url "https://github.com/carlos8f/salty/archive/v2.1.1.tar.gz"
+  version "2.1.1"
+  sha256 "0173b80427afffa114e494b8f94754a3d8cbe02497a8a892821bcadfdfa938ee"
+
+  depends_on "node"
+
+  def install
+    bin.install "salty"
+  end
+end
