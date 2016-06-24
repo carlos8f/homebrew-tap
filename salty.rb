0805264d4928b76725a721f563c7337a93010073
Carlos Rodriguez <carlos@s8f.org>
2016-06-24 00:01:26 -0700

    salty release 4.0.2

--- salty.rb
+++ salty.rb
@@ -1,9 +1,9 @@
 class Salty < Formula
   desc "Alternative public key encryption using NaCl"
   homepage "https://github.com/carlos8f/salty"
-  url "https://github.com/carlos8f/salty/archive/v4.0.0.tar.gz"
-  version "4.0.0"
-  sha256 "98d9b9a5bb6e11e1292a8153a4c8b0741bb53cefce826783297aa7df55c2f903"
+  url "https://github.com/carlos8f/salty/archive/v4.0.2.tar.gz"
+  version "4.0.2"
+  sha256 "8cac1cc6179fb05bb0e10afc26edec4ed5eb38b8bd8c7f0f4161e2e641e669d6"
 
   depends_on "node"
 
